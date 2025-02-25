c78c604286396b490422c9902db6d95c2fe00eed
Jeshersin <jeshermendieta@gmail.com>
2025-02-24 20:49:04 -0600

    01 version navbar

---
+++ app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton = () => {
+    return (
+        <div className="p-2 cursor-pointer text-smc\ font-semibold rounded-full hover:bg-gray-200 hover:text-black">
+            Djangobnb your home
+        </div>
+    );
+}
+
+export default AddPropertyButton;
