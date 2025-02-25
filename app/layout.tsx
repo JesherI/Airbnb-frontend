25980bfb9e8b1ec066ae57f28562beea318615fd
Jeshersin <jeshermendieta@gmail.com>
2025-02-25 11:52:14 -0600

    04 version add Detail page for properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        <div className="pt-14">
+        <div className="pt-32">
           {children}
         </div>
       </body>
---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import ReservationSidebar from "../../components/properties/ReservationSidebar";
+
+const PropertyDetailPage = () => {
+    return (
+        <main className="max-w-[1500px] mx-auto px-6 pb-6">
+            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                <Image fill src="/Beach-home.jpg" className="object-cover w-full h-full" alt="Beach house" />
+            </div>
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+                <div className="py-6 pr-6 col-span-3">
+                    <h1 className="mb-4 text-4xl">Property name</h1>
+                    <span className="mb-6 block text-lg text-gray-400">
+                        4 guests - 2 bedrooms - 1 bathroom
+                    </span>
+                    <hr />
+                    <div className="py-6 flex items-center space-x-4">
+                        <Image src="/profile.jpg" width={50} height={50} className="rounded-full" alt="The user name" />
+                        <p><strong>John Doe</strong> is your host</p>
+                    </div>
+                    <hr />
+                    <p className="mt-6 text-lg">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi unde inventore iste architecto delectus! Ipsum non quos mollitia quod eligendi quibusdam voluptate, autem excepturi provident inventore, laborum a ut est? 
+                    </p>
+                </div>
+                <ReservationSidebar />
+            </div>
+        </main>
+    )
+}
+
+export default PropertyDetailPage;
