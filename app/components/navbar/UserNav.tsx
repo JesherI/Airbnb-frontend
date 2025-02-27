06da674d2feec72e11f777ee8fa8b77662975888
Jeshersin <jeshermendieta@gmail.com>
2025-02-27 09:42:09 -0600

    10 version add User Menu popup

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -1,4 +1,11 @@
+'use client';
+
+import { useState } from "react";
+
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+    const [isOpen, steIsOpen] = useState(true)
     return (
         <div className="p-2 relative inline-block border rounded-full">
             <button className="flex items-center">
@@ -9,6 +16,13 @@ const UserNav = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] bg-black right-0 hober:bg-gray-900 border rounded-xl shadow-md flex flex-col cursor-pointer ">
+                    <MenuLink label='Log in'  onClick={() => console.log('Click Button')}/>
+                    <MenuLink label='Sing up' onClick={() => console.log('Click Button')}/>
+                </div>
+            )}
         </div>
     )
 }
